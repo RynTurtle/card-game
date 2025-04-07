23a5c028dd08919080a15662cda1154007261853
rynturtle <rmclaughlin6666@gmail.com>
2025-04-08 00:15:50 +0100

    initial commit

---
+++ logic.py
@@ -0,0 +1,77 @@
+
+"""
+
+the card game needs to get the players needed and deal the players cards, 5 per person 
+after the cards have been dealt start the game 
+
+{   
+    "player1":{
+        "cards":[]
+    }
+    player2:{}
+
+    wins:{player1:3,player2,2}
+}
+
+"""
+import os
+import random 
+class card_game_logic():
+    def __init__(self):
+        self.player_hands = {}
+        self.deck = [] 
+     
+
+
+    def add_player(self,player_name):
+        if len(self.player_hands.keys()) == 5:
+            return "maximum player limit reached"
+        if player_name in self.player_hands:
+            return "player already added"
+
+        self.player_hands[player_name] = {"cards":[]}
+        return f"added {player_name}"
+
+
+    # shuffle a new deck of cards
+    def shuffle_deck(self):
+        cards = os.listdir("./images/cards")
+        random.shuffle(cards)   
+        for card in cards:
+            self.deck.append(card.replace(".png",""))
+        
+    def get_card_value(self,card):
+        # card name must be formatted like the card image list 
+        face_cards = ["jack","king","queen"]
+        value = card.split("_")[0]
+        if value == "ace":
+            return 11
+        elif value in face_cards:
+            return 10
+        else:
+            return value 
+    
+
+    def deal_hands(self):
+        # need to give a card 5 times to each player 
+        for _ in range(5): 
+            for players in self.player_hands:
+                chosen_card = self.deck[0]
+                self.player_hands[players]["cards"].append(chosen_card)
+                self.deck.pop(0) # remove the card used 
+
+
+game = card_game_logic()
+
+game.add_player("ryan")
+game.add_player("jeff")
+game.add_player("jeff")
+
+game.shuffle_deck()
+game.deal_hands()
+ 
+print(game.deck)
+
+# check values of player cards, compare them, find winner 
+
+# add the 
---
+++ tests.py
@@ -0,0 +1,8 @@
+
+from logic import card_game_logic 
+
+
+# emulate the game loop that the gui will have 
+
+# main loop
+ 
