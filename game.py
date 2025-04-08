e6ff69fd5d8b87ed5f4c592a8afa3c2765abc36c
rynturtle <rmclaughlin6666@gmail.com>
2025-04-09 00:25:05 +0100

    first stages of game gui

---
+++ game.py
@@ -0,0 +1,42 @@
+import pygame 
+
+pygame.init()
+display_info = pygame.display.Info()
+display_w = display_info.current_w
+display_h = display_info.current_h
+
+screen = pygame.display.set_mode((display_w,display_h),pygame.RESIZABLE)
+clock = pygame.time.Clock()
+
+card_height = 250
+card_width = 200 
+padding = 50
+
+
+running = True
+while running:
+    # poll for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # user wants to quit
+            running = False
+
+
+    screen.fill((0, 100, 0))
+    pygame.draw.line(screen, "white", (display_w /2, 0), (display_w / 2, display_info.current_h), width=2)
+
+    #x,y,width,height,line width
+    # place a card in the middle of the screen, height is the screens size - the card height with padding added
+    pygame.draw.rect(screen, "red", pygame.Rect((display_w / 2) - (card_width / 2) , display_h - (card_height + padding), card_width, card_height), width=5) 
+    
+    # right of the middle 
+    pygame.draw.rect(screen, "red", pygame.Rect((display_w / 2) - (card_width / 2) + padding + card_width, display_h - (card_height + padding), card_width, card_height), width=5) 
+ 
+    # left of the middle 
+    pygame.draw.rect(screen, "red", pygame.Rect((display_w / 2) - (card_width / 2) - padding - card_width, display_h - (card_height + padding), card_width, card_height), width=5) 
+
+    # update game 
+    pygame.display.flip()
+    # fps
+    clock.tick(60)  
+
+pygame.quit()
