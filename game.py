8de70e1a1726b3fe88b7ae8d0224eb27233d514b
rynturtle <rmclaughlin6666@gmail.com>
2025-04-09 21:52:51 +0100

    adding to a function

--- game.py
+++ game.py
@@ -6,11 +6,29 @@ display_w = display_info.current_w
 display_h = display_info.current_h
 
 screen = pygame.display.set_mode((display_w,display_h),pygame.RESIZABLE)
+#screen = pygame.display.set_mode((1920,1080),pygame.RESIZABLE)
+
 clock = pygame.time.Clock()
 
 card_height = 250
 card_width = 200 
 padding = 50
+
+def draw_card_placeholders():
+    #x,y,width,height,line width
+    # dealer rectangle 
+    pygame.draw.rect(screen, "red", pygame.Rect((display_w / 2) - (card_width / 2) , 100, card_width, card_height), width=5) 
+
+    # place a card in the middle of the screen, the height is the screens size - the card height with padding added
+    pygame.draw.rect(screen, "red", pygame.Rect((display_w / 2) - (card_width / 2) , display_h - (card_height + padding), card_width, card_height), width=5) 
+    
+    # right of the middle 
+    pygame.draw.rect(screen, "red", pygame.Rect((display_w / 2) - (card_width / 2) + padding + card_width, display_h - (card_height + padding), card_width, card_height), width=5) 
+    pygame.draw.rect(screen, "red", pygame.Rect((display_w / 2) - (card_width / 2) + (padding *2) + (card_width*2), display_h - (card_height + padding), card_width, card_height), width=5) 
+
+    # left of the middle 
+    pygame.draw.rect(screen, "red", pygame.Rect((display_w / 2) - (card_width / 2) - padding - card_width, display_h - (card_height + padding), card_width, card_height), width=5) 
+    pygame.draw.rect(screen, "red", pygame.Rect((display_w / 2) - (card_width / 2) - (padding *2) - (card_width*2), display_h - (card_height + padding), card_width, card_height), width=5) 
 
 # todo make it responsive using 0.10 for 10% instead 
 
@@ -29,22 +47,7 @@ while running:
     pygame.draw.line(screen, "white", (display_w /2, 0), (display_w / 2, display_info.current_h), width=2)
 
 
-
-
-    #x,y,width,height,line width
-    # dealer rectangle 
-    pygame.draw.rect(screen, "red", pygame.Rect((display_w / 2) - (card_width / 2) , 100, card_width, card_height), width=5) 
-
-    # place a card in the middle of the screen, height is the screens size - the card height with padding added
-    pygame.draw.rect(screen, "red", pygame.Rect((display_w / 2) - (card_width / 2) , display_h - (card_height + padding), card_width, card_height), width=5) 
-    
-    # right of the middle 
-    pygame.draw.rect(screen, "red", pygame.Rect((display_w / 2) - (card_width / 2) + padding + card_width, display_h - (card_height + padding), card_width, card_height), width=5) 
-    pygame.draw.rect(screen, "red", pygame.Rect((display_w / 2) - (card_width / 2) + (padding *2) + (card_width*2), display_h - (card_height + padding), card_width, card_height), width=5) 
-
-    # left of the middle 
-    pygame.draw.rect(screen, "red", pygame.Rect((display_w / 2) - (card_width / 2) - padding - card_width, display_h - (card_height + padding), card_width, card_height), width=5) 
-    pygame.draw.rect(screen, "red", pygame.Rect((display_w / 2) - (card_width / 2) - (padding *2) - (card_width*2), display_h - (card_height + padding), card_width, card_height), width=5) 
+    draw_card_placeholders()
 
     # update game 
     pygame.display.flip()
